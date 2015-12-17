1fb9083c70121769967789c1b1e5a30d7be415f5
alxwest <alxwest@gmail.com>
2015-12-17 18:11:53 +0000

    New module to run Nuget on Windows

---
+++ windows/win_nuget.ps1
@@ -0,0 +1,233 @@
+#!powershell
+# This file is part of Ansible
+#
+# Copyright 2015, Alex West <alxwest@gmail.com>
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# WANT_JSON
+# POWERSHELL_COMMON
+$exefolder = "$env:programdata\nuget"
+$params = Parse-Args $args;
+$result = New-Object PSObject;
+Set-Attr $result "changed" $false;
+
+$package = Get-Attr -obj $params -name name -failifempty $true -emptyattributefailmessage "missing required argument: name"
+$outputdirectory = Get-Attr -obj $params -name outputdirectory --emptyattributefailmessage "missing required argument: outputdirectory"
+if ($outputdirectory) {$outputdirectory = $outputdirectory.Tolower()}
+
+$fileconflictaction = Get-Attr -obj $params -name fileconflictaction -default "none" | ConvertTo-Bool
+
+$version = Get-Attr -obj $params -name version -default $null
+
+$source = Get-Attr -obj $params -name source -default $null
+if ($source) {$source = $source.Tolower()}
+
+
+
+$showlog = Get-Attr -obj $params -name showlog -default "false" | ConvertTo-Bool
+$state = Get-Attr -obj $params -name state -default "present"
+
+$customexe = $false
+$nugetexe = Get-Attr -obj $params -name nugetexe -default $null
+
+if ($nugetexe) 
+{
+    $nugetexe = $nugetexe.Tolower()
+    $customexe = $true
+} 
+else 
+{
+    $nugetexe = "$exefolder\\nuget.exe"
+}
+
+if ("present","absent" -notcontains $state)
+{
+    Fail-Json $result "state is $state; must be present or absent"
+}
+
+if ("overwrite","ignore","none" -notcontains $fileconflictaction)
+{
+    Fail-Json $result "fileconflictaction is $fileconflictaction; must be overwrite, ignore or none"
+}
+
+function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+if(Test-Administrator -eq $false -and ($env:path -match [regex]::escape($exefolder)) = $false )
+{      
+    #This is temporary.
+    $env:Path += ";$exefolder"
+}
+
+
+Function Nuget-Install-Upgrade
+{
+    [CmdletBinding()]
+
+    param()
+
+    $NugetAlreadyInstalled = get-command nuget -ErrorAction 0
+    if ($NugetAlreadyInstalled -eq $null)
+    {
+        #We need to install Nuget
+        New-Item -Path $exefolder  -Force -ItemType directory
+        Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"  -OutFile $nugetexe
+        if(Test-Administrator -eq $true -and ($env:path -match [regex]::escape($exefolder)) = $false)
+        {
+            [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$exefolder", [EnvironmentVariableTarget]::Machine)
+        }
+        $result.changed = $true
+        $script:executable = $nugetexe
+    }
+    else
+    {
+        $script:executable = $nugetexe
+
+        if ($customexe -eq $false -and (Get-Item $nugetexe).VersionInfo.FileVersion -notmatch '3.3.0' )
+        {
+            #TODO Upgrade nuget
+        }
+    }
+}
+
+Function Nuget-PackageVersion
+{
+    [CmdletBinding()]
+         
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$package
+    )
+
+    if($script:version -eq $null)
+    {
+       $cmd = "$executable list $package"
+       $results = invoke-expression $cmd
+       if ($LastExitCode -ne 0)
+       {
+           Set-Attr $result "nuget_error_cmd" $cmd
+           Set-Attr $result "nuget_error_log" "$results"
+       
+           Throw "Error checking package version for $package" 
+       }
+       if($results -is [system.array])
+       {
+           Set-Attr $result "nuget_error_cmd" $cmd
+           Set-Attr $result "nuget_error_log" "More that one package returned"
+       
+           Throw "Error checking package version for $package" 
+       }
+       $script:version = $results.Substring($package.Length+1)    
+    }
+    $script:version
+}
+
+
+Function Nuget-IsInstalled
+{
+    [CmdletBinding()]
+    
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$package, 
+        [Parameter(Mandatory=$true, Position=2)]
+        [string]$outputdirectory
+    )
+
+    $version = Nuget-PackageVersion $package
+
+    return Test-Path "$outputdirectory\$package.$version\*.nupkg"    
+}
+
+Function Nuget-Install 
+{
+    [CmdletBinding()]
+    
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$package,
+        [Parameter(Mandatory=$false, Position=2)]
+        [string]$version,
+        [Parameter(Mandatory=$false, Position=3)]
+        [string]$source,
+        [Parameter(Mandatory=$false, Position=4)]
+        [string]$fileconflictaction,
+        [Parameter(Mandatory=$false, Position=5)]
+        [string]$outputdirectory
+    )
+
+    if ((Nuget-IsInstalled $package $outputdirectory) -and $fileconflictaction -eq "none" )
+    {
+        return
+    }
+
+    $cmd = "$executable install $package -verbosity detailed -noninteractive"
+
+    if ($version)
+    {
+        $cmd += " -version $version"
+    }
+
+    if ($source)
+    {
+        $cmd += " -source $source"
+    }
+
+    if ($outputdirectory)
+    {
+        $cmd += " -outputdirectory $outputdirectory"
+    }
+
+    if ($fileconflictaction)
+    {
+        $cmd += " -fileconflictaction $fileconflictaction"
+    }
+    
+    $results = invoke-expression $cmd
+
+    if ($LastExitCode -ne 0)
+    {
+        Set-Attr $result "nuget_error_cmd" $cmd
+        Set-Attr $result "nuget_error_log" "$results"
+        Throw "Error installing $package" 
+    }
+
+     $result.changed = $true
+}
+
+Try
+{
+    if ($state -eq "present")
+    {
+        Nuget-Install -package $package -version $version -source $source `
+            -fileconflictaction $fileconflictaction -outputdirectory $outputdirectory 
+    }
+    elseif (Nuget-IsInstalled $package $outputdirectory)
+    {
+            Remove-Item $outputdirectory
+            $result.changed = $true
+    }      
+    
+    Exit-Json $result;
+}
+Catch
+{
+     Fail-Json $result $_.Exception.Message
+}
+
---
+++ windows/win_nuget.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2015, Alex West <alxwest@gmail.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+DOCUMENTATION = '''
+---
+module: win_nuget
+version_added: "1.9"
+short_description: Installs packages using Nuget
+description:
+    - Installs packages using Nuget (http://nuget.org/). If Nuget is missing from the system, the module will install it.
+options:
+  name:
+    description:
+      - Name of the package to be installed
+    required: true
+  state:
+    description:
+      - State of the package on the system
+    required: false
+    choices:
+      - present
+      - absent
+    default: present
+  version:
+    description:
+      - Specific version of the package to be installed. The latest version will install if none is specified
+    required: false
+    default: null
+  source:
+    description:
+      - Specify source rather than using default nuget repository
+    require: false
+    default: null
+  outputdirectory:
+    description:
+      - Specifies the directory for the created NuGet package file. If not specified, uses the current directory.
+    require: false
+    default: null 
+  fileconflictaction:
+    description:
+      - The action to take, when asked to overwrite or ignore existing files referenced by the project. Using fileconflictaction will cause ansible to always report that a change was made
+    required: false
+    choices:
+      - overwrite
+      - ignore
+	  - none
+    default: none 
+author: "Alex West (@alxwest)"
+'''
+
+EXAMPLES = '''
+  # Install package
+  win_nuget:
+    name: my.first.package
+
+  # Install package version 6.6
+  win_nuget:
+    name: my.first.package
+    version: 1.2.0
+
+  # remove package
+  win_nuget:
+    name: my.first.package
+    state: absent
+
+  # Install package from specified repository
+  win_nuget:
+    name: my.first.package
+    source: https://locahost/api/v2/
+'''
